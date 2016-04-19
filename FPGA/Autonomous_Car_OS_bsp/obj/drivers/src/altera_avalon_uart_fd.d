a632ef0a370691aa25f07a23478d6a71197b9367
luchengautonomouscar <lu.cheng@tum.de>
2016-04-19 13:22:13 +0200

    upload of FPGA

---
+++ FPGA/Autonomous_Car_OS_bsp/obj/drivers/src/altera_avalon_uart_fd.d
@@ -0,0 +1,58 @@
+obj/drivers/src/altera_avalon_uart_fd.o: \
+ drivers/src/altera_avalon_uart_fd.c HAL/inc/alt_types.h \
+ HAL/inc/sys/alt_dev.h system.h linker.h HAL/inc/sys/alt_llist.h \
+ HAL/inc/priv/alt_dev_llist.h drivers/inc/altera_avalon_uart.h \
+ HAL/inc/sys/termios.h HAL/inc/sys/alt_warning.h UCOSII/inc/os/alt_sem.h \
+ UCOSII/inc/priv/alt_sem_ucosii.h HAL/inc/includes.h HAL/inc/os_cpu.h \
+ HAL/inc/sys/alt_irq.h HAL/inc/nios2.h UCOSII/inc/os_cfg.h \
+ HAL/inc/sys/alt_alarm.h HAL/inc/sys/alt_llist.h HAL/inc/priv/alt_alarm.h \
+ UCOSII/inc/ucos_ii.h HAL/inc/os_cpu.h UCOSII/inc/os/alt_flag.h \
+ UCOSII/inc/priv/alt_flag_ucosii.h drivers/inc/altera_avalon_uart_fd.h
+
+HAL/inc/alt_types.h:
+
+HAL/inc/sys/alt_dev.h:
+
+system.h:
+
+linker.h:
+
+HAL/inc/sys/alt_llist.h:
+
+HAL/inc/priv/alt_dev_llist.h:
+
+drivers/inc/altera_avalon_uart.h:
+
+HAL/inc/sys/termios.h:
+
+HAL/inc/sys/alt_warning.h:
+
+UCOSII/inc/os/alt_sem.h:
+
+UCOSII/inc/priv/alt_sem_ucosii.h:
+
+HAL/inc/includes.h:
+
+HAL/inc/os_cpu.h:
+
+HAL/inc/sys/alt_irq.h:
+
+HAL/inc/nios2.h:
+
+UCOSII/inc/os_cfg.h:
+
+HAL/inc/sys/alt_alarm.h:
+
+HAL/inc/sys/alt_llist.h:
+
+HAL/inc/priv/alt_alarm.h:
+
+UCOSII/inc/ucos_ii.h:
+
+HAL/inc/os_cpu.h:
+
+UCOSII/inc/os/alt_flag.h:
+
+UCOSII/inc/priv/alt_flag_ucosii.h:
+
+drivers/inc/altera_avalon_uart_fd.h:
---
+++ FPGA/Autonomous_Car_OS_bsp/obj/drivers/src/altera_avalon_uart_init.d
@@ -0,0 +1,73 @@
+obj/drivers/src/altera_avalon_uart_init.o: \
+ drivers/src/altera_avalon_uart_init.c HAL/inc/sys/alt_dev.h system.h \
+ linker.h HAL/inc/sys/alt_llist.h HAL/inc/alt_types.h \
+ HAL/inc/priv/alt_dev_llist.h HAL/inc/sys/alt_irq.h HAL/inc/nios2.h \
+ HAL/inc/sys/ioctl.h HAL/inc/sys/alt_errno.h \
+ drivers/inc/altera_avalon_uart.h HAL/inc/sys/termios.h \
+ HAL/inc/sys/alt_warning.h UCOSII/inc/os/alt_sem.h \
+ UCOSII/inc/priv/alt_sem_ucosii.h HAL/inc/includes.h HAL/inc/os_cpu.h \
+ HAL/inc/sys/alt_irq.h UCOSII/inc/os_cfg.h HAL/inc/sys/alt_alarm.h \
+ HAL/inc/sys/alt_llist.h HAL/inc/priv/alt_alarm.h UCOSII/inc/ucos_ii.h \
+ HAL/inc/os_cpu.h UCOSII/inc/os/alt_flag.h \
+ UCOSII/inc/priv/alt_flag_ucosii.h drivers/inc/altera_avalon_uart_fd.h \
+ drivers/inc/altera_avalon_uart_regs.h HAL/inc/io.h HAL/inc/alt_types.h
+
+HAL/inc/sys/alt_dev.h:
+
+system.h:
+
+linker.h:
+
+HAL/inc/sys/alt_llist.h:
+
+HAL/inc/alt_types.h:
+
+HAL/inc/priv/alt_dev_llist.h:
+
+HAL/inc/sys/alt_irq.h:
+
+HAL/inc/nios2.h:
+
+HAL/inc/sys/ioctl.h:
+
+HAL/inc/sys/alt_errno.h:
+
+drivers/inc/altera_avalon_uart.h:
+
+HAL/inc/sys/termios.h:
+
+HAL/inc/sys/alt_warning.h:
+
+UCOSII/inc/os/alt_sem.h:
+
+UCOSII/inc/priv/alt_sem_ucosii.h:
+
+HAL/inc/includes.h:
+
+HAL/inc/os_cpu.h:
+
+HAL/inc/sys/alt_irq.h:
+
+UCOSII/inc/os_cfg.h:
+
+HAL/inc/sys/alt_alarm.h:
+
+HAL/inc/sys/alt_llist.h:
+
+HAL/inc/priv/alt_alarm.h:
+
+UCOSII/inc/ucos_ii.h:
+
+HAL/inc/os_cpu.h:
+
+UCOSII/inc/os/alt_flag.h:
+
+UCOSII/inc/priv/alt_flag_ucosii.h:
+
+drivers/inc/altera_avalon_uart_fd.h:
+
+drivers/inc/altera_avalon_uart_regs.h:
+
+HAL/inc/io.h:
+
+HAL/inc/alt_types.h:
