28a6d4f29b723d89b5529ceb113529f459940d59
luchengautonomouscar <lu.cheng@tum.de>
2016-04-19 12:52:55 +0200

    create main.d

---
+++ FPGA/Autonomous_Car_OS/obj/default/main.d
@@ -0,0 +1,67 @@
+obj/default/main.o: main.c ../Autonomous_Car_OS_bsp//HAL/inc/includes.h \
+ ../Autonomous_Car_OS_bsp//HAL/inc/os_cpu.h \
+ ../Autonomous_Car_OS_bsp//HAL/inc/sys/alt_irq.h \
+ ../Autonomous_Car_OS_bsp//HAL/inc/nios2.h \
+ ../Autonomous_Car_OS_bsp//HAL/inc/alt_types.h \
+ ../Autonomous_Car_OS_bsp/system.h ../Autonomous_Car_OS_bsp/linker.h \
+ ../Autonomous_Car_OS_bsp//UCOSII/inc/os_cfg.h \
+ ../Autonomous_Car_OS_bsp//HAL/inc/sys/alt_alarm.h \
+ ../Autonomous_Car_OS_bsp//HAL/inc/sys/alt_llist.h \
+ ../Autonomous_Car_OS_bsp//HAL/inc/priv/alt_alarm.h \
+ ../Autonomous_Car_OS_bsp//UCOSII/inc/ucos_ii.h \
+ ../Autonomous_Car_OS_bsp//HAL/inc/os_cpu.h ultrasound.h uart.h motor.h \
+ communication.h gps.h \
+ ../Autonomous_Car_OS_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../Autonomous_Car_OS_bsp//HAL/inc/io.h \
+ ../Autonomous_Car_OS_bsp//HAL/inc/alt_types.h \
+ ../Autonomous_Car_OS_bsp//drivers/inc/altera_avalon_timer_regs.h \
+ ../Autonomous_Car_OS_bsp//drivers/inc/altera_avalon_uart_regs.h \
+ ../Autonomous_Car_OS_bsp//HAL/inc/sys/alt_irq.h
+
+../Autonomous_Car_OS_bsp//HAL/inc/includes.h:
+
+../Autonomous_Car_OS_bsp//HAL/inc/os_cpu.h:
+
+../Autonomous_Car_OS_bsp//HAL/inc/sys/alt_irq.h:
+
+../Autonomous_Car_OS_bsp//HAL/inc/nios2.h:
+
+../Autonomous_Car_OS_bsp//HAL/inc/alt_types.h:
+
+../Autonomous_Car_OS_bsp/system.h:
+
+../Autonomous_Car_OS_bsp/linker.h:
+
+../Autonomous_Car_OS_bsp//UCOSII/inc/os_cfg.h:
+
+../Autonomous_Car_OS_bsp//HAL/inc/sys/alt_alarm.h:
+
+../Autonomous_Car_OS_bsp//HAL/inc/sys/alt_llist.h:
+
+../Autonomous_Car_OS_bsp//HAL/inc/priv/alt_alarm.h:
+
+../Autonomous_Car_OS_bsp//UCOSII/inc/ucos_ii.h:
+
+../Autonomous_Car_OS_bsp//HAL/inc/os_cpu.h:
+
+ultrasound.h:
+
+uart.h:
+
+motor.h:
+
+communication.h:
+
+gps.h:
+
+../Autonomous_Car_OS_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../Autonomous_Car_OS_bsp//HAL/inc/io.h:
+
+../Autonomous_Car_OS_bsp//HAL/inc/alt_types.h:
+
+../Autonomous_Car_OS_bsp//drivers/inc/altera_avalon_timer_regs.h:
+
+../Autonomous_Car_OS_bsp//drivers/inc/altera_avalon_uart_regs.h:
+
+../Autonomous_Car_OS_bsp//HAL/inc/sys/alt_irq.h:
